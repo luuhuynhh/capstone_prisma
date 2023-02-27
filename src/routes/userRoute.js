b9a39e4b337651dd256aa2f5f331a1a4a3098160
LUU HUYNH NGUYEN THI <luuhuynh.tkp@gmail.com>
2023-02-28 00:36:35 +0700

    upload avatar + get user detail + update user

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,12 +1,16 @@
 const express = require('express');
-const { resgister, signin, logout } = require('../controllers/userController');
+const { resgister, signin, logout, uploadAvatar, getUserDetail, updateUserDetail } = require('../controllers/userController');
 const auth = require('../middlewares/auth');
+const upload = require('../middlewares/uploadImage');
 
 const userRoute = express.Router();
 
 userRoute.post('/', resgister);
 userRoute.post('/signin', signin);
-userRoute.post('/logout', auth, logout)
+userRoute.post('/logout', auth, logout);
+userRoute.put('/upload-avatar', auth, upload.single('file'), uploadAvatar);
+userRoute.get('/:user_id', getUserDetail);
+userRoute.put('/:user_id', auth, updateUserDetail);
 
 module.exports = {
     userRoute
